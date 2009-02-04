ce34f96ed01efd9056398106fa1372e0111edaed
Christopher Petersen <cpetersen@assaydepot.com>
2009-02-03 22:35:18 -0800

    Added Milestones

---
+++ Classes/Milestone.h
@@ -0,0 +1,20 @@
+//
+//  Milestone.h
+//  lighthouse
+//
+//  Created by Christopher Petersen on 2/3/09.
+//  Copyright 2009 Assay Depot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Milestone : NSObject {
+	NSInteger milestoneID;
+	NSString *milestoneName;
+}
+
+@property (nonatomic) NSInteger milestoneID;
+@property (nonatomic, copy) NSString *milestoneName;
+
+@end
---
+++ Classes/Milestone.m
@@ -0,0 +1,21 @@
+//
+//  Milestone.m
+//  lighthouse
+//
+//  Created by Christopher Petersen on 2/3/09.
+//  Copyright 2009 Assay Depot. All rights reserved.
+//
+
+#import "Milestone.h"
+
+
+@implementation Milestone
+
+@synthesize milestoneID, milestoneName;
+
+- (void) dealloc {
+	[milestoneName release];
+	[super dealloc];
+}
+
+@end
